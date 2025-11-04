8fafbf0398e409cdf365169848a2cba9159a2e21
Lucas Matheus <lucmvl2003@gmail.com>
2025-11-04 11:07:25 -0300

    fix: security password enconder, but now with conditional on not web application

--- src/main/java/br/com/cacadoresti/saga/config/security/PasswordEncoderConfiguration.java
+++ src/main/java/br/com/cacadoresti/saga/config/security/PasswordEncoderConfiguration.java
@@ -1,12 +1,14 @@
 package br.com.cacadoresti.saga.config.security;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@ConditionalOnNotWebApplication
 public class PasswordEncoderConfiguration {
 
     @Bean
